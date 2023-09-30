8d1d57b21a54eb4539c0d694230fb8df90eee914
Danhmeo2407 <danhhoang0145@gmail.com>
2023-09-30 16:08:04 +0700

    landau

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, Login } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
